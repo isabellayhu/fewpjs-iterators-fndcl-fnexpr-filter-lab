4f92ebc909a4e52222f41268e25a8c253ddc2fa1
Isabella <isabella.y.hu@gmail.com>
2022-04-19 10:09:53 +1000

    commit tree fee7564b98ffaf7c28b6010f3662c176bc4afcd5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(list, name){
+    return list.filter(newName => {
+        return newName.toLowerCase() === name.toLowerCase();
+    });
+}
+
+function fuzzyMatch(list, name){
+    return list.filter(newName => {
+        return newName.startsWith(name);
+    });
+}
+
+function matchName(list, matchName){
+    return list.filter(({name})=>{
+        return name === matchName;
+    });
+}
